c8311171dd8d896b95786e79c6c4db66826e85f0
cybdoom <cybdoom@gmail.com>
2016-11-24 16:06:07 +0200

    Improved organization model specs

--- spec/factories/organizations.rb
+++ spec/factories/organizations.rb
@@ -1,6 +1,8 @@
 FactoryGirl.define do
+
   factory :organization do
-    
+    type { Organization.types.keys.sample }
+    pricing_policy { Organization.pricing_policies.keys.sample }
   end
 
 end
--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -3,14 +3,21 @@ require 'rails_helper'
 RSpec.describe Organization, type: :model do
   context 'Validation' do
     it 'should be invalid' do
-      expect(FactoryGirl.build(:organization)).to be_invalid
+      expect(Organization.new).to be_invalid
     end
 
     it 'should be valid' do
-      new_record = FactoryGirl.build(:organization)
-      new_record.type = described_class.types.keys.sample
-      new_record.pricing_policy = described_class.pricing_policies.keys.sample
-      expect(new_record).to be_valid
+      expect(FactoryGirl.build(:organization)).to be_valid
+    end
+  end
+
+  context 'Record creation' do
+    it 'should not create record' do
+      expect(Organization.create).not_to be_persisted
+    end
+
+    it 'should create record' do
+      expect(FactoryGirl.create(:organization)).to be_persisted
     end
   end
 end
