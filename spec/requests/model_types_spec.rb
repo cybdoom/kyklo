ae5e117db1b91c9d286e5951c2be64851b50603d
cybdoom <cybdoom@gmail.com>
2016-11-24 23:11:11 +0200

    Added spec for case when requeted model does not exist in db

--- spec/requests/model_types_spec.rb
+++ spec/requests/model_types_spec.rb
@@ -8,16 +8,23 @@ describe "Model types" do
     let (:request_format) { :json }
 
     context "GET models/:model_slug/model_types" do
+      let(:endpoint) { "/models/#{ model.model_slug }/model_types" }
 
       it 'responds with success' do
-        get "/models/#{ model.model_slug }/model_types", format: request_format
+        get endpoint, format: request_format
         expect(response).to be_success
       end
 
       it 'finds requested model correctly' do
-        get "/models/#{ model.model_slug }/model_types", format: request_format
+        get endpoint, format: request_format
         expect(assigns(:model)).not_to be_nil
         expect(assigns(:model).model_slug).to be_eql model.model_slug
+      end
+
+      it 'raise ModelNotFound exception if failed to find given model' do\
+        expect do
+          get endpoint, format: request_format
+        end.to raise_exception(ModelNotFound)
       end
 
     end
